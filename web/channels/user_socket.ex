59e4130e10120a25be12c42650c8a5c1a24ff25a
Sabit Huraira <sabitzhabit@gmail.com>
2017-10-02 15:40:59 +0700

    set heroku config

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,8 @@ defmodule Mywallet.UserSocket do
   channel "account:*", Mywallet.AccountChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
