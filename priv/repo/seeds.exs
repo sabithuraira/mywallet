dd88affdf831c604bf9a85a5e6c05ab4ce0425ca
Sabit Huraira <sabitzhabit@gmail.com>
2017-10-04 14:46:33 +0700

    seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,74 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Mywallet.Repo
+alias Mywallet.Category
+alias Mywallet.Currency
+
+Repo.insert!(%Category{
+    name: "Food",
+    note: "",
+    user_id: 0,
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Category{
+    name: "Utilities",
+    note: "",
+    user_id: 0,
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Category{
+    name: "Medical",
+    note: "",
+    user_id: 0,
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Category{
+    name: "Home/rent",
+    note: "",
+    user_id: 0,
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Currency{
+    name: "IDR",
+    note: "Indonesia",
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Currency{
+    name: "USD",
+    note: "US Dollar",
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Currency{
+    name: "SGD",
+    note: "Singapore Dollar",
+    inserted_by: 1,
+    updated_by: 1
+})
+
+
+Repo.insert!(%Currency{
+    name: "JPY",
+    note: "Japan",
+    inserted_by: 1,
+    updated_by: 1
+})
